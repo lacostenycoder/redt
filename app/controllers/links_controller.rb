2a9e68fca975bfd813caf9664c5cecdf6bfb8c3a
ajkamel <ajkamel@gmail.com>
2014-04-08 19:48:24 -0400

    add link controller

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,39 @@
+class Links < ApplicationController
+
+	def index
+		@links = Link.all
+	end
+
+	def show
+		@links = Link.find(params[:id])
+	end
+
+	def new
+		@user = Link.new(link_params)
+	end
+
+	def create
+		@link = Link.new(link_params)
+		@link.save
+	end
+
+	def edit
+		@link = Link.find(params[:id])
+	end
+
+	def update
+		@link = Link.find(params[:id])
+		@link.update(link_params)
+	end
+
+	def destroy
+		@link = Link.find(params[:id])
+		@link.destroy
+	end
+
+	private
+	def link_params
+		params.require(:link).permit(:url, :rank)
+	end
+
+end
