70e4d9423b0630c991c4c39601ab1b2ee2719182
ajkamel <ajkamel@gmail.com>
2014-04-08 17:12:39 -0400

    add rails install with users and links migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redt::Application.config.secret_key_base = 'f7b85ff4eea82190656e07d04996aff8ca87381ffb37a310bc3c306dfa62258abccbf3c0022af1ccd4a59a65e51d73229511a82268cd42d77e07531930519b57'
