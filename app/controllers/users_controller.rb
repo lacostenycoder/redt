7f8694cfa8a2ee6181a9792bf0e14a80e92c5141
lacostenycoder <djlancejordan@gmail.com>
2014-04-08 19:26:00 -0400

    skeleton done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UserController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @users = Users.find(params[:id])
+  end
+
+  def new
+    @user = User.new(user_params)
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
